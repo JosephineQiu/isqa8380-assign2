8a4809ae1ae9f33e6808123689767e8110883538
JosephineQiu <mqiu@unomaha.edu>
2019-09-12 22:00:54 -0500

    initial commit

---
+++ src/components/About.vue
@@ -0,0 +1,28 @@
+<template>
+   <body>
+    <div class="about">
+      <h1><i>This is an about page</i></h1>
+      <p class="aboutme">My name is Menglin Qiu and I'm from China. I finished my bachelor's degree on Business
+         with a minor on Accounting at Guangdong Unviersity of Foreign Studies in China. In my senior 
+         year I got the chance of being an exchange student at Bellevue University and after graduation
+          I was transferred to University of Nebraska at Omaha. I'm now enrolled in the master program of 
+          Management Information System with a concentration on Data Analytics. In my leisure time I do 
+          reading, play the violin and watch movies. One of my top strengths is my great attention to details.
+          I am always more than glad to go an extra mile if things can be accomplished in a better way.</p>
+    </div>
+    </body>
+</template>
+
+<style scoped>
+body {
+    background-color: lavenderblush;
+    height: 60%;
+}
+.aboutme {
+     float: top-center;
+     background-color: ivory;
+     font-family:'Courier New', Courier, monospace;
+     font-size: 18px;
+}
+
+</style>
